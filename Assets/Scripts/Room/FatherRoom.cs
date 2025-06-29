12fe69a00226410bfeba3bce153ea9a54dc04336
WangSimiao2000 <1872557359@qq.com>
2025-06-29 15:03:43 +0800

    feat(games): Create Grandma/Father Scene

---
+++ Assets/Scripts/Room/FatherRoom.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatherRoom : MonoBehaviour
+{
+    public void OnPaintingFinished()
+    {
+        Debug.Log("已点击画作");
+    }
+}
---
+++ Assets/Scripts/Room/GrandmaRoom.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandmaRoom : MonoBehaviour
+{
+    public void OnFireplaceFinished()
+    {
+        Debug.Log("壁炉对话已结束");
+    }
+}
